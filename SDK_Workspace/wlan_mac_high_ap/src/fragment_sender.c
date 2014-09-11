a52b137915204c17cfb94f8ba29e5eb7c3ac72db
hptruong93 <hptruong93@gmail.com>
2014-09-11 12:56:55 -0400

    Added fragment sender

---
+++ SDK_Workspace/wlan_mac_high_ap/src/fragment_sender.c
@@ -0,0 +1,78 @@
+/*
+ * fragment_sender.c
+ *
+ *  Created on: 2014-09-10
+ *      Author: Hoai Phuoc Truong
+ */
+
+#include "xil_types.h"
+#include "xintc.h"
+#include "fragment_sender.h"
+#include "warp_protocol.h"
+
+#define MAX_ETHERNET_LENGTH                           1475
+
+function_ptr_t fragment_send_eth_callback;
+
+void set_eth_pkt_send_callback(void(*callback)()) {
+	fragment_send_eth_callback = (function_ptr_t) callback;
+}
+
+void fragment_sender_initialize(void(*callback)()) {
+	set_eth_pkt_send_callback(callback);
+}
+
+u8 generate_fragment_id() {
+	static u8 fragment_id_count = 0;
+	fragment_id_count = (fragment_id_count + 1) % 255;
+	if (fragment_id_count == 255) {
+		fragment_id_count = 0;
+	}
+	return fragment_id_count;
+}
+
+//Type has to be TYPE_TRANSMIT
+void fragmentational_send(u8 subtype, u8* packet, u16 length) {
+	if (length > 1400) {
+		return;
+	}
+	static u8 warp_header[] = { 1, 0, 0x40, 0xD8, 0x55, 0x04, 0x22, 0x84, 0, 0, 0, 0, 99, 1, 1, 0, 0 };
+
+	u8 fragment_id = generate_fragment_id();
+	warp_header[SUBTYPE_INDEX] = subtype;
+
+	if (length + sizeof(warp_header) <= MAX_ETHERNET_LENGTH) {
+		//Send in one shot
+		warp_header[FRAGMENT_INFO_INDEX + FRAGMENT_ID_INDEX] = fragment_id;
+		warp_header[TRANSMIT_HEADER_INDEX + TRANSMIT_PAYLOAD_SIZE_MSB_INDEX] = (length >> 8) & 0xff;
+		warp_header[TRANSMIT_HEADER_INDEX + TRANSMIT_PAYLOAD_SIZE_LSB_INDEX] = (length) & 0xff;
+		fragment_send_eth_callback((void*) packet, length, &(warp_header[0]), sizeof(warp_header));
+	} else {
+		xil_printf("exceed %d\n", length);
+		//Fragment the packet
+		u16 fragment_max_length = MAX_ETHERNET_LENGTH - sizeof(warp_header);
+
+		u8 total_number_fragment = (length % fragment_max_length == 0) ? (length / fragment_max_length) : (length / fragment_max_length + 1);
+		u8 packet_count = 0;
+		for (; packet_count < total_number_fragment; packet_count++) {
+			//Resolve new offset
+			u16 byte_offset = packet_count * fragment_max_length;
+			warp_header[FRAGMENT_INFO_INDEX + FRAGMENT_BYTE_OFFSET_MSB_INDEX] = (byte_offset >> 8) & 0xff;
+			warp_header[FRAGMENT_INFO_INDEX + FRAGMENT_BYTE_OFFSET_LSB_INDEX] = byte_offset & 0xff;
+
+			//Resolve new length
+			u16 fragment_length = 1;
+			if (byte_offset + fragment_max_length <= length) {
+				fragment_length = fragment_max_length;
+			} else {
+				fragment_length = length - byte_offset;
+			}
+			fragment_length += FRAGMENT_INFO_LENGTH;
+			warp_header[TRANSMIT_HEADER_INDEX + TRANSMIT_PAYLOAD_SIZE_MSB_INDEX] = (fragment_length >> 8) & 0xff;
+			warp_header[TRANSMIT_HEADER_INDEX + TRANSMIT_PAYLOAD_SIZE_LSB_INDEX] = fragment_length & 0xff;
+
+			//Send
+			fragment_send_eth_callback((void*) (packet + byte_offset), fragment_length, &(warp_header[0]), sizeof(warp_header));
+		}
+	}
+}
---
+++ SDK_Workspace/wlan_mac_high_ap/src/include/fragment_sender.h
@@ -0,0 +1,15 @@
+/*
+ * fragment_sender.h
+ *
+ *  Created on: 2014-09-10
+ *      Author: Hoai Phuoc Truong
+ */
+
+#ifndef FRAGMENT_SENDER_H_
+#define FRAGMENT_SENDER_H_
+
+u8 generate_fragment_id();
+void fragmentational_send(u8 subtype, u8* packet, u16 length);
+void fragment_sender_initialize(void(*eth_send_callback)());
+
+#endif /* FRAGMENT_SENDER_H_ */
